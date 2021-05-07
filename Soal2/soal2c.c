2a90ba0e8d50c82c8327c64602ee90520c57e85d
dvjhr <2dava.aditya21@gmail.com>
2021-05-07 14:10:14 +0700

    ganti biar lebih pasti

--- Soal2/soal2c.c
+++ Soal2/soal2c.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
-#include <wait.h>
 
 int pid, status;
 int pipe1[2];
 int pipe2[2];
 
 int main() {
+	printf("Halo lur!\n");
 if (pipe(pipe1) == -1){printf("Andri"); exit(1);}
 if (pipe(pipe2) == -1){printf("Andri lagi"); exit(1);}
 
@@ -24,26 +26,33 @@ if (pipe(pipe2) == -1){printf("Andri lagi"); exit(1);}
 	else {
 
 		while(wait(&status) > 0);
+
 		if ((fork()) == 0) {
 		close(pipe1[1]);
-		dup2(pipe1[0], STDIN_FILENO);
 		close(pipe2[0]);
+		// printf("---masuk child 2\n");
+		dup2(pipe1[0], STDIN_FILENO);
 		dup2(pipe2[1], STDOUT_FILENO);
-		// exec
+		
 		char *argv1[] = {"sort", "-nrk", "3,3", NULL};
-		execv("/bin/sort", argv1);
+		execv("/usr/bin/sort", argv1);
+
 		} 
 		
 		else {
-		while(wait(&status) > 0);
 
-			// input from pipe1
+			// // input from pipe1
+			close(pipe1[0]);
+			close(pipe1[1]);
 			close(pipe2[1]);
+			while(wait(&status) > 0);
+			// printf("---masuk parent");
 			dup2(pipe2[0], STDIN_FILENO);
-
+			close(pipe2[0]);
+			
 			char *argv1[] = {"head", "-5", NULL};
 			execv("/usr/bin/head", argv1);
-
+			
 		}
 	}
 }
